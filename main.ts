789288131aec5f55e82d4e986f8b8e342205bf88
SUMO6049 <126621570+SUMO6049@users.noreply.github.com>
2023-05-04 14:43:40 +0100

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,3 @@
+scene.setBackgroundImage(assets.image`carser city`)
+scroller.setLayerImage(scroller.BackgroundLayer.Layer4, assets.image`carser city`)
+scroller.setLayerImage(scroller.BackgroundLayer.Layer0, assets.image`carser city`)
